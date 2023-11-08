307f72d4d080760e2a2ffe9c757a7dd10f3191fb
ahnafaf <ahnaf.ahsan9@gmail.com>
2023-11-07 22:54:44 -0600

    slight update

--- binaryTree.java
+++ binaryTree.java
@@ -1,72 +1,80 @@
 public class binaryTree {
     public static void main(String args[]) {
-        BinarySearchTree tree = new BinarySearchTree();
-        tree.insert(new Node(5))
+        BinarySearchTree bst = new BinarySearchTree();
+        bst.insert(new Node(5));
+        bst.insert(new Node(2));
+        bst.insert(new Node(3));
+        System.out.println("Attempting to print");
+        bst.display();
+    }
+}
 
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    public Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
     }
 
-    class Node {
-        int data;
-        Node left;
-        Node right;
+}
 
-        public Node(int data) {
-            this.data = data;
-            left = null;
-            right = null;
-        }
+class BinarySearchTree {
+    Node root;
 
+    public void insert(Node node) {
+        root = insertHelper(root, node);
     }
 
-    class BinarySearchTree {
-        Node root;
-
-        public void insert(Node node)
-        {
-            root = insertHelper(root, node);
-
-        }
-        private Node insertHelper(Node root, Node node)
-        {
-            int data = node.data;
-            if (root == null) {
-                root = node;
-                return root;
-            } else if (data < root.data) {
-                root.left = insertHelper(root.left, node);
-            } else {
-                root.right = insertHelper(root.right, node);
-            }
-
-            return root;
+    private Node insertHelper(Node root, Node node) {
+        if (root == null) {
+            root = node;
+            return node;
         }
 
-        public void display()
-        {
+        if (node.data < root.data) {
+            root.left = insertHelper(root.left, node);
+        } else if (node.data > root.data) {
+            root.right = insertHelper(root.right, node);
+        }
 
-        }
-        private void displayHelper()
-        {
+        return root;
+    }
+    public void display()
+    {
+        displayHelper(root);
+    }
+    private void displayHelper(Node root)
+    {
+        if(root != null) {
+            displayHelper(root.left);
+            System.out.println(root.data);
+            displayHelper(root.right);
 
-        }
-        public boolean search(int data)
-        {
-            return false;
-        }
-        private boolean searchHelper(Node root, int data)
-        {
-            return false;
-        }
+        } }
+    public boolean search(int data)
+    {
+
+        return false; }
+    private boolean searchHelper(Node root, int data)
+    {
+
+        return false;
+    }
+
         public void remove(int data) {
-        }
-        private void removeHelper(Node root, int data)
-        {
-            return;
-        }
+    }
 
-        private int successor(Node root)
-        {
-            return 0;
-        }
+    private void removeHelper(Node root, int data)
+    {     return;
+    }
+
+    private int successor(Node root)
+    {
+        return 0;
     }
 }
